61e737f6330b82c377c4fe2116de186d6a1b0a08
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-11-13 22:40:22 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/veber/lesson1mvplite/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.gb.veber.lesson1mvplite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

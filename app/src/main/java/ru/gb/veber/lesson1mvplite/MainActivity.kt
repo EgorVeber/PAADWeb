eb8984f52a0c2e8350d18534628a69090c5b1b56
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-11-22 22:11:17 +0300

    example flow

--- app/src/main/java/ru/gb/veber/lesson1mvplite/MainActivity.kt
+++ app/src/main/java/ru/gb/veber/lesson1mvplite/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.gb.veber.lesson1mvplite
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.*
@@ -17,10 +18,62 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        getFlow()
+//        getFlow()
+//        startFlow()
+
+
+        setupFlows()
+        zipFlows()
+
     }
 
-    private fun getFlow() {
+    private fun getFlow() = flow {
+        logD("Start Flow")
+        (0..10).forEach {
+            delay(500)
+            logD("Emmitting $it")
+            emit(it)
+        }
+    }.flowOn(Dispatchers.Default)
 
+    private fun startFlow() {
+        binding.button.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                getFlow().collect{
+                    logD(it)
+                }
+            }
+        }
+    }
+
+    lateinit var flowOne: Flow<String>
+    lateinit var flowTwo: Flow<String>
+
+
+    private fun setupFlows() {
+        flowOne = flowOf("Юрий", "Александр", "Иван").flowOn(Dispatchers.Default)
+        flowTwo = flowOf("Гагарин", "Пушкин", "Грозный").flowOn(Dispatchers.Default)
+    }
+    private fun zipFlows() {
+        findViewById<Button>(R.id.button).setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                flowOne.zip(flowTwo)
+                { firstString, secondString ->
+                    "$firstString $secondString"
+                }.collect {
+                    logD(it)
+                }
+            }
+        }
+    }
+
+
+
+    companion object {
+        const val LOG_COROUTINE_TAG = "LOG_COROUTINE_TAG"
+    }
+
+    private fun logD(message: Any) {
+        Log.d(LOG_COROUTINE_TAG, message.toString())
     }
 }

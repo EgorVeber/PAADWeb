0bc5158e5652d512477dd6baaf4c1865879907ea
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-11-22 19:28:48 +0300

    flow

--- app/src/main/java/ru/gb/veber/lesson1mvplite/MainActivity.kt
+++ app/src/main/java/ru/gb/veber/lesson1mvplite/MainActivity.kt
@@ -1,10 +1,17 @@
 package ru.gb.veber.lesson1mvplite
 
+import android.location.GnssAntennaInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import ru.gb.veber.lesson1mvplite.databinding.ActivityMainBinding
+import java.util.concurrent.CopyOnWriteArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -25,21 +32,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var flow = flow {
+            repeat(10) {
+                delay(400)
+                emit(it)
+            }
+        }
 
-        val flag = true
+        var flowList = flowOf(listOf(1, 2, 3, 4), listOf(1, 2, 3, 4))
+
+        var flowListExtension = listOf(1, 2, 3, 4).asFlow()
+
         binding.buttonStart.setOnClickListener {
             job?.cancel()
             job = coroutineScope.launch {
-                while (isActive) {
-                    delay(400)
-                    printLog(isActive)
-                    printLog(job?.isCancelled.toString())
+                flowList.collect {
+                    printLog(it)
                 }
             }
         }
 
         binding.buttonStop.setOnClickListener {
-            if (flag) job?.cancel() else job?.start()
+            job?.cancel()
         }
     }
 
@@ -49,5 +63,32 @@ class MainActivity : AppCompatActivity() {
 
     private fun printLog(message: Any) {
         Log.d(LOG_COROUTINE_TAG, message.toString())
+    }
+
+    private fun callBackFlow(someCallbackClass: SomeCallbackClass) = callbackFlow {
+        var numder = 1000
+        var listener = SomeCallbackClass.Listener {
+            trySend(numder++)
+        }
+        someCallbackClass.addListener(listener)
+        awaitClose { someCallbackClass.remove(listener) }
+    }
+}
+
+class SomeCallbackClass() {
+    fun interface Listener {
+        fun onChange()
+    }
+
+    val listeners = CopyOnWriteArrayList<Listener>()
+
+    fun addListener(listener: Listener) {
+        listeners.add(listener)
+    }
+
+    fun remove(listener: Listener) = listeners.remove(listener)
+
+    fun invoke() {
+        listeners.forEach { it.onChange() }
     }
 }

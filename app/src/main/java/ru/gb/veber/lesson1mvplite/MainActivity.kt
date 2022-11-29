6b7fad0c1151b871dc375070659fcc5f1bb4aee5
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-11-29 20:54:23 +0300

    Stopwatch Example

--- app/src/main/java/ru/gb/veber/lesson1mvplite/MainActivity.kt
+++ app/src/main/java/ru/gb/veber/lesson1mvplite/MainActivity.kt
@@ -5,10 +5,16 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import androidx.work.Worker
-import kotlin.concurrent.thread
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val errorHandler = CoroutineExceptionHandler { point1, error ->
+        Log.d("WORKER", "null() called with: _ = $point1 error = $error")
+    }
+
+    private val coroutineScope = CoroutineScope(Dispatchers.IO + errorHandler)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,14 +27,15 @@ class MainActivity : AppCompatActivity() {
 //            .then(myWorkRequest2)
 //            .enqueue()
 
-        val myWorkRequest = OneTimeWorkRequestBuilder<MyWorker>().build()
+        val myWorkRequest = OneTimeWorkRequestBuilder<MyWorker>().addTag("MyTag").build()
         WorkManager.getInstance(this).enqueue(myWorkRequest)
 
         WorkManager.getInstance(this).getWorkInfoByIdLiveData(myWorkRequest.id).observe(this) {
             Log.d("WORKER", "onChanged: " + it.state)
         }
-
-
-        WorkManager.getInstance(this).cancelAllWork()
+        coroutineScope.launch {
+            delay(2000)
+            WorkManager.getInstance(this@MainActivity).cancelAllWorkByTag("MyTag")
+        }
     }
 }
